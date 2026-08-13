e76d374a306faf9c5ac6a278af73077d9c9ae18d
Suhai <thegui4000@gmail.com>
2026-08-13 15:34:01 -0300

    feat<login>: get login page from shadcn

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ import "./app.css"
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="pt-br">
             <head>
                 <meta charSet="utf-8" />
                 <meta
